92d553ed74a70b87844528a6bb00c7b99ddf0f6b
NahuelQuesada <nahueldiazquesada@gmail.com>
2025-11-20 20:24:46 -0300

    Inicializo el proyecto

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+
+const  port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+
+
+
+
+
